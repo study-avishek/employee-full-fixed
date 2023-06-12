1083c135717ca5c26ded65473749a723c13d3481
Avishek Chatterjee <avishekchatterjee@N0474-AvishekChatterjee.local>
2023-06-12 14:42:28 +0530

    first commit

--- src/main/java/com/increff/employee/spring/DbConfig.java
+++ src/main/java/com/increff/employee/spring/DbConfig.java
@@ -68,7 +68,6 @@ public class DbConfig {
 		jpaProperties.put("hibernate.show_sql", hibernateShowSql);
 		jpaProperties.put("hibernate.hbm2ddl.auto", hibernateHbm2ddl);
 		jpaProperties.put("hibernate.hbm2ddl.auto", hibernateHbm2ddl);
-		bean.setJpaProperties(jpaProperties);
 		return bean;
 	}
 	
